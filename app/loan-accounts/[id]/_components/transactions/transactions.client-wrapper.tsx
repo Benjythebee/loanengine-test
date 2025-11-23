d0c9ca3a70a6ccf5cfe11bf1377fd09497c6dcb7
Benjythebee <Benjy.larcher@aol.com>
2025-11-24 08:51:05 +1300

    better type prop

--- app/loan-accounts/[id]/_components/transactions/transactions.client-wrapper.tsx
+++ app/loan-accounts/[id]/_components/transactions/transactions.client-wrapper.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { type TransactionQueryData } from "@/hooks/useTransactionQuery";
+import { ComponentProps } from "react";
 import { TransactionsDataTable } from "./transactions.table.client";
 
-interface TransactionsClientWrapperProps {
-  loanId: string;
-  initialData?: TransactionQueryData;
-  pagination: { pageIndex: number; pageSize: number };
-  error?: string;
-}
+
 /**
  * Transaction Table Client Wrapper to force client-side rendering
  * attempts solve https://github.com/Benjythebee/loanengine-test/issues/1
  */
-export function TransactionsClientWrapper(props: TransactionsClientWrapperProps) {
+export function TransactionsClientWrapper(props: ComponentProps<typeof TransactionsDataTable>) {
   return <TransactionsDataTable {...props} />;
 }
